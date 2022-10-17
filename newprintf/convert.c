19f8b63d80f8b072f9d506db5cb256481c02b3bd
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-10-17 19:38:57 +0100

    mandatory task

---
+++ newprintf/convert.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char *convert(int num, int base)
+{
+    static char *Representation = "0123456789ABCDEF";
+    static char buffer[50];
+    char *ptr;
+    ptr = &buffer[49];
+    *ptr = '\0';
+
+    do
+    {
+        *--ptr =  Representation[num%base];
+        num/=base;
+    }while (num != 0);
+    
+    return (ptr);
+
+}
